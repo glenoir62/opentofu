8112455c3b48bd269e678136d778eecb7f4b0cc9
Guillaume Lenoir <guillaume.lenoir@ext.adeo.com>
2025-11-28 16:45:16 +0100

    Add AWS provider, S3 bucket resource, and Terraform output in CDKTF stack
    - Configure AWS provider with specific region and profile.
    - Add S3 bucket resource with tags and unique name.
    - Include Terraform output for bucket name in stack.

--- projet2-cdktf/main.ts
+++ projet2-cdktf/main.ts
@@ -1,14 +1,37 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket";
+// Importer les constructs pour le provider AWS depuis le répertoire .gen/
 
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+class MyDemoStack extends TerraformStack {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
 
-    // define resources here
-  }
+        // Définir le provider AWS
+        new AwsProvider(this, "MonProviderAWS", {
+            region: "eu-west-3",
+            profile: "projet1-sso", // Votre profil AWS CLI
+        });
+
+        // Exemple : créer un bucket S3
+        const monBucket = new S3Bucket(this, "MonPremierBucket", {
+            bucket: "mon-premier-bucket-cdktf-unique-xxxx", // REMPLACEZ par un nom globalement unique !
+            tags: {
+                Name: "MonPremierBucketCDKTF",
+                Environment: "DemoCDKTF",
+            },
+        });
+
+        // Exemple d'output
+        new TerraformOutput(this, "NomDuBucket", {
+            value: monBucket.bucket,
+        });
+    }
 }
 
 const app = new App();
-new MyStack(app, "projet2-cdktf");
+new MyDemoStack(app, "projet2-cdktf"); // Nom de la stack qui apparaîtra dans cdktf.out
+
+// Synthétiser la configuration Terraform JSON
 app.synth();
